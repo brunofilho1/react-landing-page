f62508128acc0556e7a546555a7f854c056e0949
Bruno Oliveira <brunowalker551@gmail.com>
2021-11-05 20:35:14 -0300

    added phone input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/global.scss'
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/index";
 import { Main } from './components/Main';
 import { Fragment } from 'react';
 import { Footer } from './components/Footer';
